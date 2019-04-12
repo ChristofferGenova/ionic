44800233c79dd8d54c452c6c7b3a0412e39c5f8f
christoffergenova <christoffergenova1@gmail.com>
2019-04-12 08:33:23 -0400

    first commit

---
+++ src/pages/list/list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { ListPage } from './list';
+import { IonicPageModule } from 'ionic-angular';
+
+@NgModule({
+  declarations:  [
+    ListPage]
+    ,
+  imports: [
+    IonicPageModule.forChild(ListPage)
+  ],
+})
+export class ListModule { }
